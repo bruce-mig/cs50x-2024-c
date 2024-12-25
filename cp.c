44de1437a0230b2310daa97bd1fd32acbe2ade4d
bruce-mig <brucetmig@icloud.com>
2024-12-25 22:24:44 +0200

    custom cp command to copy files

--- cp.c
+++ cp.c
@@ -5,7 +5,17 @@ typedef uint8_t BYTE;
 
 int main(int argc, char *argv[]) {
     FILE *src = fopen(argv[1], "rb");
+    if (src == NULL) {
+        perror("Error opening file");
+        return 1;
+    }
+    
     FILE *dst = fopen(argv[2], "wb");
+     if (src == NULL) {
+        perror("Error opening file");
+        return 1;
+    }
+    
 
     BYTE b;
 
