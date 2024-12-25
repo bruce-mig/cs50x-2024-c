07ca43304c6a36cf046f56e869b7449019774863
bruce-mig <brucetmig@icloud.com>
2024-12-25 22:05:51 +0200

    addressed potential seg faults

--- get_str.c
+++ get_str.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void) {
-    // char *s; // Segmentation fault (core dumped)
-    
     char s[4];
-    printf("s: "); 
-    scanf("%s: ", s); // s: HI! else *** stack smashing detected ***: terminated
-    printf("s: %s\n", s);
+
+    printf("s: ");
+    if (fgets(s, sizeof(s), stdin) != NULL) {
+        // Remove the newline character if present
+        size_t len = strlen(s);
+        if (len > 0 && s[len - 1] == '\n') {
+            s[len - 1] = '\0';
+        }
+        printf("s: %s\n", s);
+    } else {
+        printf("Error reading input.\n");
+    }
+
+    return 0;
 }
