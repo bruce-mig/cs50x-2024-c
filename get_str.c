7be00ef724685b2e61bf5ac4265486ce97318532
bruce-mig <brucetmig@icloud.com>
2024-12-25 22:41:51 +0200

    remove the newline character if present

--- get_str.c
+++ get_str.c
@@ -6,7 +6,6 @@ int main(void) {
 
     printf("s: ");
     if (fgets(s, sizeof(s), stdin) != NULL) {
-        // Remove the newline character if present
         size_t len = strlen(s);
         if (len > 0 && s[len - 1] == '\n') {
             s[len - 1] = '\0';
