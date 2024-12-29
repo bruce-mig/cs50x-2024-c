4bb549c1ffd72fecf4cae36d24b4f6d981f0bdf7
bruce-mig <brucetmig@icloud.com>
2024-12-29 19:29:10 +0200

    FIFO linked list

--- linked_list.c
+++ linked_list.c
@@ -1,4 +1,5 @@
-// Implements a list of numbers using a linked list
+// Implements a list of numbers using a linked list.
+// Each number is added to the end of the list, preserving the input order.
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -25,7 +26,7 @@ int main(int argc, char *argv[]) {
         n->next = NULL;
 
         // If list is empty
-        if (list = NULL) {
+        if (list == NULL) {
             // This node is the whole list
             list = n;
         }
@@ -42,4 +43,18 @@ int main(int argc, char *argv[]) {
             }
         }
     }
+
+    // Print the list
+    for (node *ptr = list; ptr != NULL; ptr = ptr->next) {
+        printf("%d\n", ptr->number);
+    }
+
+    // Free the list
+    while (list != NULL) {
+        node *tmp = list->next;
+        free(list);
+        list = tmp;
+    }
+
+    return 0;
 }
